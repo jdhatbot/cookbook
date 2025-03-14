1aebf000681c337271eba288cb17fd025b1df7ff
Daniel <dhattemer097@gmail.com>
2025-03-13 21:30:21 -0500

    Removed a lot of stuff

--- cookbooksite/src/main.js
+++ cookbooksite/src/main.js
@@ -3,8 +3,25 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { createVuetify } from 'vuetify'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+
+import '@mdi/font/css/materialdesignicons.css'
 
 const app = createApp(App)
+
+const vuetify = createVuetify({
+  theme: { defaultTheme: 'dark' },
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+})
+
+app.use(vuetify)
 
 app.use(router)
 
