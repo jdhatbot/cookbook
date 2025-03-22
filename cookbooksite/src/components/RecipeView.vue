cf215e01eef6667b27d35683b583893d1e604e77
Daniel <dhattemer097@gmail.com>
2025-03-21 21:37:35 -0500

    Removed starter components and got the recipeView to look okay

--- cookbooksite/src/components/RecipeView.vue
+++ cookbooksite/src/components/RecipeView.vue
@@ -1,18 +1,59 @@
 <template>
     <div>
       <v-container>
-       
+        <v-row>
+          <v-col cols="12" align="center">
+            <h1>{{ recipe.name }}</h1>
+          </v-col>
+        </v-row>
+        <!-- <v-row>
+          <v-col cols="12" align="center">
+            <v-img
+              :src="getImage(recipe.image)"
+              height="400"
+              width="400"
+              class="mx-auto"
+            ></v-img>
+          </v-col>
+        </v-row> -->
+        <v-row>
+          <v-col cols="12" align="center">
+            <h3>{{ recipe.type }}</h3>
+          </v-col>
+        </v-row>
+       <v-row>
+        <v-col>
+          <v-card>
+            <v-card-title>Ingredients</v-card-title>
+            <v-card-text>
+              <v-list>
+                <v-list-item v-for="ingredient in recipe.ingredients" :key="ingredient.index">
+                  
+                    <v-list-item-title class="text-wrap">{{ ingredient.ingredientText }}</v-list-item-title>
+                  
+                </v-list-item>
+              </v-list>
+            </v-card-text>
+          </v-card>
+        </v-col>
+        <v-col>
+          <v-card>
+            <v-card-title>Instructions</v-card-title>
+            <v-card-text>
+              <v-list>
+                <v-list-item v-for="(instruction, index) in recipe.directions" :key="index">
+                  
+                    <v-list-item-title class="text-wrap">{{ instruction }}</v-list-item-title>
+                  
+                </v-list-item>
+              </v-list>
+            </v-card-text>
+          </v-card>
+        </v-col>
+       </v-row>
       </v-container>  
       
-      <h1>{{ recipe.name }}</h1>
-      <v-img
-        :src="getImage(recipe.image)"
-        height="400"
-        width="400"
-        class="mx-auto"
-      ></v-img>
-      <h3>{{ recipe.type }}</h3>
-      
+
   </div>
   </template>
   
@@ -28,11 +69,6 @@ const props = defineProps({id: String});
   const recipe = ref(computed(() => {
     return recipes.value.find((recipe) => recipe.id === props.id);
   }).value);
-
-
-
-
-
 
 const getImage = (image) => {
   return new URL(`../recipeImages/${image}`, import.meta.url).href;
